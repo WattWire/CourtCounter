332a8330e923707a316d7ab0c72976ffcf7ec685
Dave Watt <gonebikn@gmail.com>
2016-04-07 13:03:54 -0700

    First LapCat commit...at Peets and finally got the Layout looking good.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,23 +7,48 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(6);
+
+        displayForTeamA(scoreTeamA);
     }
 
     public void addThreeForTeamA(View v){
-        displayForTeamA(3);
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addTwoForTeamA(View v){
-        displayForTeamA(2);
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void addOneForTeamA(View v){
-        displayForTeamA(1);
+    public void addOneForTeamA(View v) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+//==========
+
+    public void addThreeForTeamB(View v){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View v){
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+
     }
 
     /**
@@ -33,4 +58,13 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
